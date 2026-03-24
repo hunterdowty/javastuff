79559af80b767a95576e98c697f5b45d627a65f6
Hunter Dowty <hddowty42@tntech.edu>
2026-03-23 22:35:08 -0500

    first programs

---
+++ ShowFirstMessage.java
@@ -0,0 +1,8 @@
+
+public class ShowFirstMessage {
+
+    public void displayMessage() {
+        System.out.println("This is my first message.");
+    }
+
+}
---
+++ UseShowFirstMessage.java
@@ -0,0 +1,8 @@
+public class UseShowFirstMessage{
+    public static void main(String[] args){
+        ShowFirstMessage messageOne;
+
+        messageOne = new ShowFirstMessage();
+        messageOne.displayMessage();
+    }
+}

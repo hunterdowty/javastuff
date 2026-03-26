ebd160dec9f8fc6097f0e4573b261c570f2c9ecf
Hunter Dowty <hddowty42@tntech.edu>
2026-03-25 22:25:00 -0500

    more java practice

---
+++ ShowEscapeSequences.java
@@ -0,0 +1,9 @@
+public class ShowEscapeSequences {
+    public static void main(String[] args) {
+        System.out.println("This is a message with escape sequences:");
+        System.out.println("New line:\nThis is on a new line.");
+        System.out.println("Tab:\tThis is after a tab.");
+        System.out.println("Backslash:\\ This is a backslash.");
+        System.out.println("Double quote: \"This is in double quotes.\"");
+    }
+}
